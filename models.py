3e9e27ef42c373f67c269abad6c0fdba8c92b603
eititan <john_hutchins@comcast.net>
2020-03-11 05:23:21 -0600

    added model and api routes

---
+++ models.py
@@ -0,0 +1,11 @@
+# PYTHON CONSOLE STUFF
+
+# from test import Pokemon
+#
+# bulbasaur = Pokemon(1, 'bulbasaur')
+# ivysaur = Pokemon(2, 'ivysaur')
+# venusaur = Pokemon(3, 'venusaur')
+# .
+# .
+# .
+# mew = Pokemon(151, 'mew')
